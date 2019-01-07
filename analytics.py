e907839ff7dc0c2f3d195a6734edf4bbe44d20a1
Andrew Moss <andrew.moss@edu.sait.ca>
2019-01-06 18:49:31 -0800

    Finally got chart to render on webpage

--- analytics.py
+++ analytics.py
@@ -4,6 +4,7 @@ import time
 import pandas as pd
 import mysql.connector
 import folium
+import json
 
 from scripts import Query as q
 from scripts import Database
@@ -60,6 +61,31 @@ def main(df):
     print("Skew: {}".format(df['price'].skew()))
     # # Normally Distributed? I guess...
 
+def visuals(df):
+
+    # Instantiate Objects
+    sp = StaticPlot.StaticPlot(df,PATHS['write'])
+    ply = PlotlyPlots.PlotlyPlots(df,PATHS['write'])
+
+    static_bar_community = sp.bar_community()
+    static_box_price_quadrant = sp.boxplot_price_quadrant()
+    static_dist_price = sp.distplot_price()
+    static_corr_heat = sp.corr_heat()
+
+    dynamic_bar_community = ply.bar_community()
+    dynamic_box_price = ply.box_price_quadrant()
+    dynamic_dist_price = ply.distplot()
+    
+    data = {}
+
+    data['dynamic_bar_community'] = str(dynamic_bar_community)
+    data['dynamic_box_price'] = str(dynamic_box_price)
+    data['dynamic_dist_price'] = str(dynamic_dist_price)
+
+    with open(PATHS['write'] + 'plots.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+
 if __name__ == "__main__":
 
     conn = Database.Database.connect()
@@ -68,4 +94,4 @@ if __name__ == "__main__":
 
     df = get.data_for_analysis()
 
-    main(df)
+    visuals(df)
--- djsite/rental/views.py
+++ djsite/rental/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings # For file paths
+
+import json
+import os
+
 
 def index(request):
-    return render(request,'rental/index.html')
+
+    title = {
+        'title' : 'Dashboard'
+    }
+
+    file_path = os.path.join(settings.VISUALS_DIR, 'plots.json')
+
+    with open(file_path) as f:
+        data = json.load(f)
+
+    return render(request,'rental/index.html', {'title': title, 'data' : data})
 
 def about(request):
-    return render(request,'rental/about.html')
+
+    title = {
+        'title' : 'About'
+    }
+
+    return render(request,'rental/about.html',title)
 
 def data(request):
-    return render(request,'rental/data.html')
+
+    title = {
+        'title' : 'Data'
+    }
+
+    return render(request,'rental/data.html',title)
