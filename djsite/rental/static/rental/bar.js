244996879f5e91dc656827fa67fd189dc7fb23c1
andrew <agordonmoss@gmail.com>
2019-01-17 11:21:46 -0700

    D3 bar chart

--- djsite/rental/static/rental/bar.js
+++ djsite/rental/static/rental/bar.js
@@ -1,77 +1,75 @@
 // set the dimensions of the canvas
 var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+  width = 1200 - margin.left - margin.right,
+  height = 800 - margin.top - margin.bottom;
 
 
 // set the ranges
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
 
 var y = d3.scale.linear().range([height, 0]);
 
 // define the axis
 var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-
+  .scale(x)
+  .orient("bottom")
 
 var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
-
-
+  .scale(y)
+  .orient("left")
+  .ticks(10);              
+              
 // add the SVG element
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("#svg").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform", 
+        "translate(" + margin.left + "," + margin.top + ")");
 
 
 // load the data
-d3.json("data.json", function(error, data) {
+d3.json("{% url 'box_data' %}", function(error, data) {
 
-    data.forEach(function(d) {
-        d.Letter = d.Letter;
-        d.Freq = +d.Freq;
-    });
-	
-  // scale the range of the data
-  x.domain(data.map(function(d) { return d.Letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.Freq; })]);
+  data.forEach(function(d) {
+      d.community = d.community;
+      d.dcount = +d.dcount;
+  });
 
-  // add axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
+// scale the range of the data
+x.domain(data.map(function(d) { return d.community; }));
+y.domain([0, d3.max(data, function(d) { return d.dcount; })]);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 5)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
+// add axis
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+  .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", "-.55em")
+    .attr("transform", "rotate(-90)" );
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 5)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Frequency");
 
 
-  // Add bar chart
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.Letter); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.Freq); })
-      .attr("height", function(d) { return height - y(d.Freq); });
+// Add bar chart
+svg.selectAll("bar")
+    .data(data)
+  .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.community); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d) { return y(d.dcount); })
+    .attr("height", function(d) { return height - y(d.dcount); });
 
 });
--- djsite/rental/views.py
+++ djsite/rental/views.py
@@ -1,15 +1,18 @@
+# Standard django view imports
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings # For file paths
 
-# Testing
+# For api data
 from django.db import connections
 from django.db.models import Count
 from django.http import JsonResponse
+from rental.models import RentalData
+from django.core import serializers
 
+# Python & third party imports
 import json
 import os
-
 import folium
 
 
@@ -60,24 +63,22 @@ def calgary_heat_map(request):
     return render(request,'rental/calgary_heat_map.html',title)
 
 
-# Testing
-
-from rental.models import RentalData
-
-def box_chart(request):
-    return render(request, 'rental/graph_test.html')
-
-
 def box_data(request):
+    """ JSON API """
 
-    from django.core import serializers
+    # GROUP BY
+    data = list(
+        RentalData.objects.using('rental_data')
+        .values('community')
+        .annotate(dcount=Count('community'))
+        .order_by('-dcount')
+        .filter(dcount__gt=30)
+        )
 
-    entries = RentalData.objects.only('community').using('rental_data')
-
-    entries = list(entries.values())
-
+    # SELECT *
     #data = list(RentalData.objects.using('rental_data').values())
 
-    return JsonResponse(entries, safe=False)  # or JsonResponse({'data': data})
+    # assuming obj is a model instance (manual serialization)
+    #serialized_obj = serializers.serialize('json', count )
 
-
+    return JsonResponse(data, safe=False)  # or JsonResponse({'data': data})
